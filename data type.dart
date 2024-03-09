c4e1a073534ea17632cfc9c328d2d8fd5fdc8c1c
itumbi <imanase37@gmail.com>
2024-03-09 13:59:45 -0800

    data type commit

---
+++ data type.dart
@@ -0,0 +1,21 @@
+void main() {
+  //integer application in addition
+  int num1=20; //whole numbers
+  int num2=15; //whole numbers
+  int sum= num1+num2;
+  print("The sum of two integers is $sum");
+  //double application in multiplication
+  double num3=10.5; //decimal numbers
+  double num4=2.7; //decimal numbers
+  double mult=num3*num4; 
+  print("The product of double is $mult");
+  //string application in printing name
+  String name="Joshua";
+  print("My name is $name");
+  //list application in secondary school subjects
+  List subjects=['physics','chemistry','biology','history','mathematics'];
+  print("Secondary school subjects are $subjects");
+  //map application in capital cities 
+  Map<String,String> capitalCity={"Tanzania":"dodoma","Kenya":"nairobi","Uganda":"kampala"};
+  print("Capital cities of east african countries are $capitalCity");
+}
